e4f99c6447e6b4fa2cd60b4c0a60cce4f611a417
Mayukh Das <mayukh4321@gmail.com>
2026-04-19 20:58:17 +0800

    pretty glaring mistake in weird algo

--- Introductory Problems/01 - Weird Algorithm.rs
+++ Introductory Problems/01 - Weird Algorithm.rs
@@ -4,7 +4,7 @@ pub fn solve() {
     pp!(@it std::iter::successors(Some(a),
         |&x| (x != 1)
             .then_some(
-                (x & 1 != 0)
+                (x & 1 == 0)
                     .then_some(x >> 1)
                     .unwrap_or(3 * x + 1)
             )
